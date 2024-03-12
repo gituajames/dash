daa8f87bd987fb1a369c0bf4483b9caab763acda
gituajames <gitua85@gmail.com>
2024-03-12 09:51:33 +0300

    added a float field

--- dashmon/models.py
+++ dashmon/models.py
@@ -10,5 +10,5 @@ class Wifis(models.Model):
 
 class PingTime(models.Model):
     ssid = models.CharField(max_length=100)
-    ping_time = models.IntegerField()
+    ping_time = models.FloatField(default=0.0)
     ping_date_time = models.DateTimeField(auto_now=True)
